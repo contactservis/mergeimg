9562e5b207fbce31f5d85a75224c4505d99ddfe6
contactservis <skiminok2005@gmail.com>
2018-12-26 00:35:55 +0300

    Кнопки захвата фото

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
     <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <input type="file" name="image" accept="image/*" capture="environment">
+                <h4>"Селфи"</h4>
+                <input type="file" name="image" accept="image/*" capture="user">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
